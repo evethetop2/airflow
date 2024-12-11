860da1978c1fe5b741cf2b92c39dfcdd476b32ee
hkpark <iamhyeongkyu0912@naver.com>
2024-12-11 21:57:52 +0900

    python op_arg 설정

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -1,12 +1,12 @@
 from airflow.models.dag import DAG
 import pendulum
 from airflow.operators.python import PythonOperator
-from airflow.decorators import task
+from common.common_regist import regist
 
 with DAG(
-    dag_id="dag_python_task_decorator",
+    dag_id="dags_python_with_op_args",
     #시간임
-    schedule="0 2 * * *",
+    schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz="Asia/Seoul"),
     #누락된거 돌릴거냐 안돌릴거냐, 차례차례 안돌고 한번에 돌게됨
     catchup=False,
@@ -14,9 +14,10 @@ with DAG(
     # tags=["example", "example2"],
     # params={"example_key": "example_value"},
 ) as dag:
+    regist_t1 = PythonOperator(
+        task_id = 'regist_t1',
+        python_callable=regist,
+        op_args=['hkpark','man','kr','seoul']
+    )
 
-   @task(task_id="python_task_1")
-   def print_context(some_input):
-        print(some_input)
-
-   python_task_1 = print_context("task_decorator 실행")
+    regist_t1
