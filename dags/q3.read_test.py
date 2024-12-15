674dae9daeda24f6675ad269f6b154f1fdbf700f
hkpark <iamhyeongkyu0912@naver.com>
2024-12-15 15:48:38 +0900

    'test5'

--- dags/q3.read_test.py
+++ dags/q3.read_test.py
@@ -1,41 +1,32 @@
 from airflow import DAG
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.operators.python import PythonOperator
-from sqlalchemy import create_engine
+from datetime import datetime, timedelta
 import pandas as pd
 from datetime import datetime
-import pymysql
 
-# 기본 설정
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2024, 12, 15),
-    'retries': 1,
-}
+def read_table():
+    mysql_hook = MySqlHook(mysql_conn_id='hyperconnect_1')
+    sql = """
+    select *
+    from table_a
+    ;
+    """
+    a= mysql_hook.run(sql)
+    return a
 
-def read_mysql_table_with_sqlalchemy(**kwargs):
-    # SQLAlchemy 연결 URL 형식
-    connection_string = "mysql+pymysql://hyperconnect_1:gksdud12@183.96.150.41:3306/hyperconnect"
-    
-    # SQLAlchemy 엔진 생성
-    engine = create_engine(connection_string)
-    
-    # 쿼리 작성
-    query = "SELECT * FROM table_a"
-    
-    # 쿼리 실행 및 결과를 Pandas DataFrame으로 가져오기
-    df = pd.read_sql(query, engine)
-    
-    # 결과 출력
-    print(df.head())
+with DAG(
+    dag_id='read_table_using_hook',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
 
-# DAG 정의
-with DAG('mysql_read_sqlalchemy_dag',
-         default_args=default_args,
-         schedule_interval='@daily',  # 매일 실행
-         catchup=False) as dag:
-
-    read_task = PythonOperator(
-        task_id='read_mysql_table_with_sqlalchemy',
-        python_callable=read_mysql_table_with_sqlalchemy,
-        provide_context=True
+    read_table_task = PythonOperator(
+        task_id='read_table_task',
+        python_callable=read_table,
+        retries=1,  # Manually set retry parameters
+        retry_delay=timedelta(minutes=5),
     )
+
+    read_table_task
