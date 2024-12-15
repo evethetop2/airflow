68095491887d75a2e2739695ef41f8212c30eb96
hkpark <iamhyeongkyu0912@naver.com>
2024-12-15 17:00:03 +0900

    test2gd

--- dags/q3.read_test.py
+++ dags/q3.read_test.py
@@ -21,13 +21,13 @@ def process_dataframe(**kwargs):
     df_json = kwargs['ti'].xcom_pull(key='df', task_ids='read_table_task')
 
     # JSON을 다시 DataFrame으로 변환
-    df = pd.read_json(df_json, orient='split')
-    df[['id', 'user_name']] = df['value'].apply(lambda x: pd.Series(json.loads(x)))
-    df.drop('value', axis=1, inplace=True)
-    df['dt'] = pd.to_datetime(df['dt'], format='%Y/%m/%d')
+    # df = pd.read_json(df_json, orient='split')
+    df_json[['id', 'user_name']] = df_json['value'].apply(lambda x: pd.Series(json.loads(x)))
+    df_json.drop('value', axis=1, inplace=True)
+    df_json['dt'] = pd.to_datetime(df_json['dt'], format='%Y/%m/%d')
 
     print("Received DataFrame:")
-    print(df)
+    print(df_json)
 
 
 with DAG(
